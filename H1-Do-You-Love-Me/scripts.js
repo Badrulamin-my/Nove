57b4ebfecd561b6cd118848018d3b8581b366ec0
MUHAMAD BADRUL AMIN <102642059+Badrulamin-my@users.noreply.github.com>
2024-05-21 17:09:41 +0800

    Update scripts.js

--- H1-Do-You-Love-Me/scripts.js
+++ H1-Do-You-Love-Me/scripts.js
@@ -1,14 +1,21 @@
-// Homework 1 (web development with JavaScript)
+// Homework 3: Do you love me? [2]
 
-// Requirments
+// Requirements
+// there should be 2 stages to this homework
+
+// Stage 1:
 // 1- move 'NO' button to a random location on the screen when havering overit with the mouse or when clicking
+// 2- show an alert to the user when clicking "YES" button with the message "I love you too!!"
+// 3- after certen number of times the 'NO' button should be positioned above the 'YES' button and we start stage 2
+
+// Stage 2:
+// 1- when hovering over the 'NO' button the button should go below the 'YES' button with 200 ms delay
 // 2- show an alert to the user when clicking "YES" button with the message "I love you too!!"
 
 // Dont edit the .html or .css files
 
 // ##########################################
-// write your code here
-
+// write your code here 
 document.addEventListener('DOMContentLoaded', function() {
     const noButton = document.getElementById('no-button');
     const yesButton = document.getElementById('yes-button');
